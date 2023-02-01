6866392515c1cc21f0f13bcc75e784a1fc985a11
Ido Berkovich <idoberko2@gmail.com>
2023-02-01 22:26:04 +0200

    improved api key fetch

--- background.js
+++ background.js
@@ -1,26 +1,37 @@
 chrome.webRequest.onSendHeaders.addListener(
-    (details) => {
+    async details => {
         console.debug('getting credentials...');
         if (!details.requestHeaders) {
             return;
         }
 
-        const credentials = {};
-        details.requestHeaders.forEach(header => {
-            if (header.name.toLowerCase() == 'application-api-key') {
+        const credentials = await loadCredentialsFromStorage() || {};
+        let updatedApiKey = false;
+        for (header of details.requestHeaders) {
+            if (header.name.toLowerCase() == 'application-api-key' && header.value != credentials.appApiKey) {
                 credentials.appApiKey = header.value;
-                console.debug('got app api key');
+                console.debug('got fresh app api key');
+                updatedApiKey = true;
             }
+        }
 
-            if (!credentials.appApiKey) {
-                console.debug('did not find appApiKey header');
-                return;
-            }
+        if (!updatedApiKey) {
+            console.debug('did not find appApiKey header');
+            return;
+        }
 
-            chrome.storage.local.set({ credentials }, () => {
-                console.debug('credentials set');
-                chrome.runtime.sendMessage({ type: 'CREDENTIALS_SET' }, (response) => {});
-            });
+        chrome.storage.local.set({ credentials }, () => {
+            console.debug('credentials set');
+            chrome.runtime.sendMessage({ type: 'CREDENTIALS_SET' }, (response) => {});
         });
     },
     { urls: ['https://central.myvisit.com/CentralAPI/*'] }, ['requestHeaders', 'extraHeaders']);
+
+function loadCredentialsFromStorage() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get('credentials', data => {
+            console.debug('received credentials from storage', data.credentials);
+            resolve(data.credentials);
+        });
+    })
+}
